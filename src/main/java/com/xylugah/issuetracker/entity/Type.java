4bba3c4ca8aceca2252856bb0caa9ddd346cd04f
XylugaH <XylugaH3k@gmail.com>
2017-03-24 15:32:01 +0300

    Implements the methods.

--- src/main/java/com/xylugah/issuetracker/entity/Type.java
+++ src/main/java/com/xylugah/issuetracker/entity/Type.java
@@ -1,5 +1,53 @@
 package com.xylugah.issuetracker.entity;
 
-public class Type {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="Type")
+public class Type {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "Name")
+	private String name;
+	
+	public Type() {
+	}
+
+	public Type(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Type [name=" + name + "]";
+	}
+	
+	
+	
+	
+	
 }
