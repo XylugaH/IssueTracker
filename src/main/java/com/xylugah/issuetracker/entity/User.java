52dfd48c386b9ba4ddfc41407e6367bf1c4bcf05
XylugaH <XylugaH3k@gmail.com>
2017-03-24 11:33:04 +0300

    Add the constructors and getters-setters methods.

--- src/main/java/com/xylugah/issuetracker/entity/User.java
+++ src/main/java/com/xylugah/issuetracker/entity/User.java
@@ -1,5 +1,71 @@
 package com.xylugah.issuetracker.entity;
 
 public class User {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String emailAddress;
+	private Role role;
+	
+	public User() {
 
+	}
+
+	public User(final String firstName, 
+			final String lastName, 
+			final String password, 
+			final String emailAddress, 
+			final Role role) {
+		
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+		this.emailAddress = emailAddress;
+		this.role = role;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+	
+	
+	
 }
