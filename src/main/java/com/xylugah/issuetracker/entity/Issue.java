3fa2f73d5637a8300ac0a5c30fad62db257b764b
XylugaH <XylugaH3k@gmail.com>
2017-03-24 19:01:06 +0300

    Add hibernate's annotations

--- src/main/java/com/xylugah/issuetracker/entity/Issue.java
+++ src/main/java/com/xylugah/issuetracker/entity/Issue.java
@@ -1,5 +1,13 @@
 package com.xylugah.issuetracker.entity;
 
 public class Issue {
-
+	private int id;
+	
+	private String summary;
+	private String description;
+	private Status status;
+	private Type type;
+	private Priority priority;
+	private Project project;
+	
 }
