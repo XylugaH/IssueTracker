79a81dd70401ea89228d019cc33ecd669e63d4cb
XylugaH <XylugaH3k@gmail.com>
2017-03-25 17:15:38 +0300

    Implements the status class.

--- src/main/java/com/xylugah/issuetracker/entity/Status.java
+++ src/main/java/com/xylugah/issuetracker/entity/Status.java
@@ -1,5 +1,51 @@
 package com.xylugah.issuetracker.entity;
 
-public class Status {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="Status")
+public class Status {
+	
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="name", nullable=false)
+	private String name;
+	
+	public Status() {
+		super();
+	}
+
+	public Status(final String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Status [name=" + name + "]";
+	}	
+	
 }
