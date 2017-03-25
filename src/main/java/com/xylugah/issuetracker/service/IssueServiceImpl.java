7caf88db2b996c2ccd44dd8fb13f5de7b06d4b91
XylugaH <XylugaH3k@gmail.com>
2017-03-25 16:24:35 +0300

    Add the IssueServiceImpl.

---
+++ src/main/java/com/xylugah/issuetracker/service/IssueServiceImpl.java
@@ -0,0 +1,40 @@
+package com.xylugah.issuetracker.service;
+
+import java.util.List;
+
+import com.xylugah.issuetracker.entity.Issue;
+import com.xylugah.issuetracker.entity.User;
+
+public class IssueServiceImpl implements IssueService{
+
+	@Override
+	public Issue getById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Issue> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(Issue issue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User edit(Issue issue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
