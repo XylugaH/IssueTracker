0db94795d1a239d8962d6f3ddb78390fe3718a2a
XylugaH <XylugaH3k@gmail.com>
2017-03-25 10:19:56 +0300

    Add the UserServiceImpl

---
+++ src/main/java/com/xylugah/issuetracker/service/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.xylugah.issuetracker.service;
+
+import java.util.List;
+
+import com.xylugah.issuetracker.entity.User;
+
+public class UserServiceImpl implements UserService{
+
+	@Override
+	public User getById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User edit(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
