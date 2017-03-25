117d21c814080b803621354b2c88afbf96297899
XylugaH <XylugaH3k@gmail.com>
2017-03-25 16:15:30 +0300

    Add the issueController

---
+++ src/main/java/com/xylugah/issuetracker/controller/IssueController.java
@@ -0,0 +1,15 @@
+package com.xylugah.issuetracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IssueController {
+	
+	@RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+	public String issueList(ModelMap model){
+		return "issueList";
+	}
+}
