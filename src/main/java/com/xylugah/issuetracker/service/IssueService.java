86f90e4add424536ec82ac5a0519afa679bca074
XylugaH <XylugaH3k@gmail.com>
2017-03-25 16:24:13 +0300

    Add and implements the interface IssueService

---
+++ src/main/java/com/xylugah/issuetracker/service/IssueService.java
@@ -0,0 +1,14 @@
+package com.xylugah.issuetracker.service;
+
+import java.util.List;
+
+import com.xylugah.issuetracker.entity.Issue;
+import com.xylugah.issuetracker.entity.User;
+
+public interface IssueService {
+	Issue getById(int id);
+	List<Issue> getAll();
+	void add(Issue issue);
+	void delete(int id);
+	User edit(Issue issue);
+}
