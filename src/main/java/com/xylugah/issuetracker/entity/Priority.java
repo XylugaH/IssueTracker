ff1afedbb92749930542721943e9dceb437e04d9
XylugaH <XylugaH3k@gmail.com>
2017-03-25 17:24:49 +0300

    Implements the priority class.

--- src/main/java/com/xylugah/issuetracker/entity/Priority.java
+++ src/main/java/com/xylugah/issuetracker/entity/Priority.java
@@ -1,5 +1,52 @@
 package com.xylugah.issuetracker.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Priority")
 public class Priority {
+	
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="name", nullable=false)
+	private String name;
+	
+	public Priority() {
+		super();
+	}
+
+	public Priority(final String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Status [name=" + name + "]";
+	}	
+	
 
 }
