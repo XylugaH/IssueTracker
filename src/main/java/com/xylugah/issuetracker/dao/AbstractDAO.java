7eb37f5e3666f3eaed263f40502c0cbe6373e9ec
XylugaH <XylugaH3k@gmail.com>
2017-03-26 09:10:29 +0300

    Add the class.

---
+++ src/main/java/com/xylugah/issuetracker/dao/AbstractDAO.java
@@ -0,0 +1,7 @@
+package com.xylugah.issuetracker.dao;
+
+import com.xylugah.issuetracker.entity.AbstractEntity;
+
+public abstract class AbstractDAO<PK extends java.io.Serializable, T extends AbstractEntity> {
+
+}
