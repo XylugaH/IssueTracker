db5d9043cc1c0dd8e98231a3d7c0879e49004ef8
XylugaH <XylugaH3k@gmail.com>
2017-03-26 09:19:45 +0300

    Add session factory.

--- src/main/java/com/xylugah/issuetracker/dao/AbstractDAO.java
+++ src/main/java/com/xylugah/issuetracker/dao/AbstractDAO.java
@@ -1,7 +1,15 @@
 package com.xylugah.issuetracker.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 import com.xylugah.issuetracker.entity.AbstractEntity;
 
 public abstract class AbstractDAO<PK extends java.io.Serializable, T extends AbstractEntity> {
-
+	
+	private SessionFactory sessionFactory;
+	
+	protected Session getSession(){
+		return sessionFactory.getCurrentSession();
+	}
 }
