59658a00eee3f7b5c61633173a34dfc480f39128
XylugaH <XylugaH3k@gmail.com>
2017-03-26 09:12:32 +0300

    Add the class.

---
+++ src/main/java/com/xylugah/issuetracker/dao/UserDaoImpl.java
@@ -0,0 +1,13 @@
+package com.xylugah.issuetracker.dao;
+
+import com.xylugah.issuetracker.entity.User;
+
+public class UserDaoImpl extends AbstractDAO<Integer,User> implements UserDAO{
+
+	@Override
+	public User findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
