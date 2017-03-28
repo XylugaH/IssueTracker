18d87a0ba366716aba78c690e34b9791e2eb316b
XylugaH <XylugaH3k@gmail.com>
2017-03-28 12:57:58 +0200

    Implements a CRUD methods.

--- src/main/java/com/xylugah/issuetracker/dao/ResolutionDAO.java
+++ src/main/java/com/xylugah/issuetracker/dao/ResolutionDAO.java
@@ -1,5 +1,19 @@
 package com.xylugah.issuetracker.dao;
 
-public interface ResolutionDAO {
+import java.util.List;
 
+import com.xylugah.issuetracker.entity.Resolution;
+
+public interface ResolutionDAO {
+	
+	Resolution getById(int id);
+	
+	List<Resolution> getAll();
+	
+	void add(Resolution resolution);
+	
+	void delete(int id);
+	
+	Resolution edit(Resolution resolution);	
+	
 }
--- src/main/java/com/xylugah/issuetracker/dao/ResolutionDAOImpl.java
+++ src/main/java/com/xylugah/issuetracker/dao/ResolutionDAOImpl.java
@@ -1,7 +1,45 @@
 package com.xylugah.issuetracker.dao;
 
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
 import com.xylugah.issuetracker.entity.Resolution;
 
+@Repository("ResolutionDAO")
 public class ResolutionDAOImpl extends AbstractDAO<Integer, Resolution> implements ResolutionDAO{
 
+	@Override
+	public Resolution getById(int id) {
+		Resolution resolution = (Resolution) getSession().get(Resolution.class, id);
+		return resolution;
+	}
+
+	@SuppressWarnings({ "unchecked", "deprecation" })
+	@Override
+	public List<Resolution> getAll() {
+		List<Resolution> resolutionList = getSession().createCriteria(Resolution.class).list();
+		return resolutionList;
+	}
+
+	@Override
+	public void add(Resolution resolution) {
+		getSession().saveOrUpdate(resolution);
+	}
+
+	@Override
+	public void delete(int id) {
+		Resolution resolution = getById(id);
+		if (resolution!=null){
+			getSession().delete(resolution);
+		}
+	}
+
+	@Override
+	public Resolution edit(Resolution resolution) {
+		getSession().update(resolution);
+		return resolution;
+	}
+
+	
 }
