426f13f81b3aa7178234a0f673453fed9471852c
XylugaH <XylugaH3k@gmail.com>
2017-03-28 11:26:12 +0200

    Implements a CRUD methods.

--- src/main/java/com/xylugah/issuetracker/dao/StatusDAO.java
+++ src/main/java/com/xylugah/issuetracker/dao/StatusDAO.java
@@ -1,5 +1,13 @@
 package com.xylugah.issuetracker.dao;
 
-public interface StatusDAO {
+import java.util.List;
 
+import com.xylugah.issuetracker.entity.Status;
+
+public interface StatusDAO {
+	Status getById(int id);
+	List<Status> getAll();
+	void add(Status status);
+	void delete(int id);
+	Status edit(Status status);
 }
--- src/main/java/com/xylugah/issuetracker/dao/StatusDAOImpl.java
+++ src/main/java/com/xylugah/issuetracker/dao/StatusDAOImpl.java
@@ -1,7 +1,44 @@
 package com.xylugah.issuetracker.dao;
 
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
 import com.xylugah.issuetracker.entity.Status;
 
+@Repository("StatusDAO")
 public class StatusDAOImpl extends AbstractDAO<Integer,Status> implements StatusDAO{
+
+	@Override
+	public Status getById(int id) {
+		Status status = (Status) getSession().get(Status.class, id);
+		return status;
+	}
+
+	@SuppressWarnings({ "unchecked", "deprecation" })
+	@Override
+	public List<Status> getAll() {
+		List<Status> statusList = getSession().createCriteria(Status.class).list();
+		return statusList;
+	}
+
+	@Override
+	public void add(Status status) {
+		getSession().saveOrUpdate(status);
+	}
+
+	@Override
+	public void delete(int id) {
+		Status status = getById(id);
+		if (status!=null){
+			getSession().delete(status);
+		}
+	}
+
+	@Override
+	public Status edit(Status status) {
+		getSession().update(status);
+		return status;
+	}
 
 }
