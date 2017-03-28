72a740ba600d6692db21ef1aed443787edc182b6
XylugaH <XylugaH3k@gmail.com>
2017-03-28 13:00:29 +0200

    Formatting .

--- src/main/java/com/xylugah/issuetracker/dao/StatusDAO.java
+++ src/main/java/com/xylugah/issuetracker/dao/StatusDAO.java
@@ -5,9 +5,15 @@ import java.util.List;
 import com.xylugah.issuetracker.entity.Status;
 
 public interface StatusDAO {
+	
 	Status getById(int id);
+	
 	List<Status> getAll();
+	
 	void add(Status status);
+	
 	void delete(int id);
+	
 	Status edit(Status status);
+	
 }
