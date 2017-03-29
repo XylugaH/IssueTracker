4c50d53e22be7102f8231261e216b49cff15a25e
XylugaH <XylugaH3k@gmail.com>
2017-03-29 20:10:27 +0300

    Add and implement service.

--- src/main/java/com/xylugah/issuetracker/service/IssueService.java
+++ src/main/java/com/xylugah/issuetracker/service/IssueService.java
@@ -6,9 +6,15 @@ import com.xylugah.issuetracker.entity.Issue;
 import com.xylugah.issuetracker.entity.User;
 
 public interface IssueService {
+	
 	Issue getById(int id);
+	
 	List<Issue> getAll();
+	
 	void add(Issue issue);
+	
 	void delete(int id);
+	
 	User edit(Issue issue);
+	
 }
