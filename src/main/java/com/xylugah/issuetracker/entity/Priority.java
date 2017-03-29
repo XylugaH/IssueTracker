62b4ddca8209d90f47854d394c0f5d4a035de92b
XylugaH <XylugaH3k@gmail.com>
2017-03-29 20:11:14 +0300

    Add valid annotations.

--- src/main/java/com/xylugah/issuetracker/entity/Priority.java
+++ src/main/java/com/xylugah/issuetracker/entity/Priority.java
@@ -6,6 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @Table(name="Priority")
@@ -16,6 +19,8 @@ public class Priority extends AbstractEntity{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
+	@NotEmpty
+	@Size(min=3, max=45)
 	@Column(name="name", nullable=false)
 	private String name;
 	
