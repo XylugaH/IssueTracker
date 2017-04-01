5e7267747bc94ae646877891e3543a785cbfe2c0
XylugaH <XylugaH3k@gmail.com>
2017-04-01 12:21:03 +0300

    Create and implements the DAO to role.

--- src/main/java/com/xylugah/issuetracker/dao/RoleDAO.java
+++ src/main/java/com/xylugah/issuetracker/dao/RoleDAO.java
@@ -1,5 +1,18 @@
 package com.xylugah.issuetracker.dao;
 
-public interface RoleDAO {
+import java.util.List;
 
+import com.xylugah.issuetracker.entity.Role;
+
+public interface RoleDAO {
+	
+	Role getById(int id);
+	
+	List<Role> getAll();
+	
+	void add(Role role);
+	
+	void delete(int id);
+	
+	Role edit(Role role);	
 }
--- src/main/java/com/xylugah/issuetracker/dao/RoleDAOImpl.java
+++ src/main/java/com/xylugah/issuetracker/dao/RoleDAOImpl.java
@@ -1,7 +1,44 @@
 package com.xylugah.issuetracker.dao;
 
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
 import com.xylugah.issuetracker.entity.Role;
 
+@Repository("RoleDAO")
 public class RoleDAOImpl extends AbstractDAO<Integer, Role> implements RoleDAO{
 
+	@Override
+	public Role getById(int id) {
+		Role role = (Role) getSession().get(Role.class, id);
+		return role;
+	}
+
+	@SuppressWarnings({ "unchecked", "deprecation" })
+	@Override
+	public List<Role> getAll() {
+		List<Role> roleList = getSession().createCriteria(Role.class).list();
+		return roleList;
+	}
+
+	@Override
+	public void add(Role role) {
+		getSession().saveOrUpdate(role);
+	}
+
+	@Override
+	public void delete(int id) {
+		Role role = getById(id);
+		if (role!=null){
+			getSession().delete(role);
+		}
+	}
+
+	@Override
+	public Role edit(Role role) {
+		getSession().update(role);
+		return role;
+	}
+	
 }
