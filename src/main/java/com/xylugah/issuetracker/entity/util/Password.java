17c80390371e392cbbaace13101d4169cfed4bfc
XylugaH <XylugaH3k@gmail.com>
2017-04-10 20:46:06 +0300

    Add util class.

---
+++ src/main/java/com/xylugah/issuetracker/entity/util/Password.java
@@ -0,0 +1,25 @@
+package com.xylugah.issuetracker.entity.util;
+
+public class Password {
+	
+	private String password;
+	
+	private String passwordConfirm;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordConfirm() {
+		return passwordConfirm;
+	}
+
+	public void setPasswordConfirm(String passwordConfirm) {
+		this.passwordConfirm = passwordConfirm;
+	}
+	
+}
