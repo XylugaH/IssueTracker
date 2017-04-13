02a26bb1c22c449b97c228909d9695879cb8c28c
XylugaH <XylugaH3k@gmail.com>
2017-04-13 18:06:51 +0300

    Move insert statements from CreateDB to InitDataDB.

--- CreateDB.sql
+++ CreateDB.sql
@@ -14,17 +14,12 @@ CREATE TABLE  user (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-INSERT INTO  user VALUES (1,"admin","admin","$2a$11$MmNXphksqwYLE.eMGXW60eQgUnxlc6BtrvLeSYhrK42BkwJpJXcWC","admin@admin.com",2);
-
 CREATE TABLE  role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(45) NOT NULL
 )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
-
-INSERT INTO  role VALUES (1,"ROLE_USER");
-INSERT INTO  role VALUES (2,"ROLE_ADMIN");
 
 CREATE TABLE  status (
   id INT NOT NULL AUTO_INCREMENT,
@@ -34,24 +29,12 @@ CREATE TABLE  status (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-INSERT INTO  status VALUES (1,"New");
-INSERT INTO  status VALUES (2,"Assigned");
-INSERT INTO  status VALUES (3,"In Progress");
-INSERT INTO  status VALUES (4,"Resolved");
-INSERT INTO  status VALUES (5,"Closed");
-INSERT INTO  status VALUES (6,"Reopened");
-
 CREATE TABLE  resolution (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(45) NULL,
   PRIMARY KEY (id))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
-
-INSERT INTO  resolution VALUES (1,"Fixed");
-INSERT INTO  resolution VALUES (2,"Invalid");
-INSERT INTO  resolution VALUES (3,"Wontfix");
-INSERT INTO  resolution VALUES (4,"Worksforme");
 
 CREATE TABLE  priority (
   id INT NOT NULL AUTO_INCREMENT,
@@ -60,11 +43,6 @@ CREATE TABLE  priority (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-INSERT INTO  priority VALUES (1,"Critical");
-INSERT INTO  priority VALUES (2,"Major");
-INSERT INTO  priority VALUES (3,"Important");
-INSERT INTO  priority VALUES (4,"Minor");
-
 CREATE TABLE  type (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(45) NOT NULL,
@@ -72,11 +50,6 @@ CREATE TABLE  type (
   UNIQUE INDEX id_UNIQUE (id ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
-
-INSERT INTO  type VALUES (1,"Cosmetic");
-INSERT INTO  type VALUES (2,"Bug");
-INSERT INTO  type VALUES (3,"Feature");
-INSERT INTO  type VALUES (4,"Performance");
 
 CREATE TABLE issues (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---
+++ InitDataDB.sql
@@ -0,0 +1,27 @@
+INSERT INTO  user VALUES (1,"admin","admin","$2a$11$MmNXphksqwYLE.eMGXW60eQgUnxlc6BtrvLeSYhrK42BkwJpJXcWC","admin@admin.com",2);
+
+INSERT INTO  role VALUES (1,"ROLE_USER");
+INSERT INTO  role VALUES (2,"ROLE_ADMIN");
+
+INSERT INTO  status VALUES (1,"New");
+INSERT INTO  status VALUES (2,"Assigned");
+INSERT INTO  status VALUES (3,"In Progress");
+INSERT INTO  status VALUES (4,"Resolved");
+INSERT INTO  status VALUES (5,"Closed");
+INSERT INTO  status VALUES (6,"Reopened");
+
+INSERT INTO  resolution VALUES (1,"Fixed");
+INSERT INTO  resolution VALUES (2,"Invalid");
+INSERT INTO  resolution VALUES (3,"Wontfix");
+INSERT INTO  resolution VALUES (4,"Worksforme");
+
+INSERT INTO  priority VALUES (1,"Critical");
+INSERT INTO  priority VALUES (2,"Major");
+INSERT INTO  priority VALUES (3,"Important");
+INSERT INTO  priority VALUES (4,"Minor");
+
+INSERT INTO  type VALUES (1,"Cosmetic");
+INSERT INTO  type VALUES (2,"Bug");
+INSERT INTO  type VALUES (3,"Feature");
+INSERT INTO  type VALUES (4,"Performance");
+
