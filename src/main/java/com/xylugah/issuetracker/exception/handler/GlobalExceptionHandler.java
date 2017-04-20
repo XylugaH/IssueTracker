3602d5826efffa40426ae6b8527ec05a4f331916
XylugaH <XylugaH3k@gmail.com>
2017-04-20 10:41:42 +0300

    Refactoring.

--- src/main/java/com/xylugah/issuetracker/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/xylugah/issuetracker/exception/handler/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.xylugah.issuetracker.exception.handler;
 
+import java.util.Locale;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +22,15 @@ import com.xylugah.issuetracker.exception.UserNotFoundException;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
+	
+	@Autowired
+	ReloadableResourceBundleMessageSource messageSource;
+	
 	@ExceptionHandler(UserNotFoundException.class)
 	public ModelAndView userNotFoundExceptionHandler(UserNotFoundException e) {
 		logger.error("Error: ", e);
 		ModelAndView model = new ModelAndView("page404");
-		model.addObject("message", "User Not Found!!");
+		model.addObject("message", messageSource.getMessage("User.not.found", null, Locale.getDefault()));
 		return model;
 	}
 
@@ -31,7 +38,7 @@ public class GlobalExceptionHandler {
 	public ModelAndView typeNotFoundExceptionHandler(TypeNotFoundException e) {
 		logger.error("Error: ", e);
 		ModelAndView model = new ModelAndView("page404");
-		model.addObject("message", "Type Not Found!!");
+		model.addObject("message", messageSource.getMessage("Type.not.found", null, Locale.getDefault()));
 		return model;
 	}
 	
@@ -39,7 +46,7 @@ public class GlobalExceptionHandler {
 	public ModelAndView statusNotFoundExceptionHandler(StatusNotFoundException e) {
 		logger.error("Error: ", e);
 		ModelAndView model = new ModelAndView("page404");
-		model.addObject("message", "Status Not Found!!");
+		model.addObject("message", messageSource.getMessage("Status.not.found", null, Locale.getDefault()));
 		return model;
 	}
 
@@ -47,7 +54,7 @@ public class GlobalExceptionHandler {
 	public ModelAndView resolutionNotFoundExceptionHandler(ResolutionNotFoundException e) {
 		logger.error("Error: ", e);
 		ModelAndView model = new ModelAndView("page404");
-		model.addObject("message", "Resolution Not Found!!");
+		model.addObject("message", messageSource.getMessage("Resolution.not.found", null, Locale.getDefault()));
 		return model;
 	}
 	
@@ -55,7 +62,7 @@ public class GlobalExceptionHandler {
 	public ModelAndView projectNotFoundExceptionHandler(ProjectNotFoundException e) {
 		logger.error("Error: ", e);
 		ModelAndView model = new ModelAndView("page404");
-		model.addObject("message", "Project Not Found!!");
+		model.addObject("message", messageSource.getMessage("Project.not.found", null, Locale.getDefault()));
 		return model;
 	}
 	
@@ -64,7 +71,7 @@ public class GlobalExceptionHandler {
 	public ModelAndView priorityNotFoundExceptionHandler(PriorityNotFoundException e) {
 		logger.error("Error: ", e);
 		ModelAndView model = new ModelAndView("page404");
-		model.addObject("message", "Priority Not Found!!");
+		model.addObject("message", messageSource.getMessage("Priority.not.found", null, Locale.getDefault()));
 		return model;
 	}
 	
@@ -72,7 +79,7 @@ public class GlobalExceptionHandler {
 	public ModelAndView issueNotFoundExceptionHandler(IssueNotFoundException e) {
 		logger.error("Error: ", e);
 		ModelAndView model = new ModelAndView("page404");
-		model.addObject("message", "Issue Not Found!!");
+		model.addObject("message", messageSource.getMessage("Issue.not.found", null, Locale.getDefault()));
 		return model;
 	}
 	
@@ -80,7 +87,7 @@ public class GlobalExceptionHandler {
 	public ModelAndView handleOException() {
 		logger.error("Error: ");
 		ModelAndView model = new ModelAndView("page404");
-		model.addObject("message", "Page Not Found!!");
+		model.addObject("message", messageSource.getMessage("Page.not.found", null, Locale.getDefault()));
 		return model;
 	}
 }
